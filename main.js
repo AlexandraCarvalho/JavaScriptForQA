f4c0e769c6c503f1b9325cc8263b26e853891ad6
Alexandra <alexandracarvalho0202@gmail.com>
2020-05-11 18:20:07 -0300

    envio dos primeiros arquivos

---
+++ main.js
@@ -0,0 +1,19 @@
+var welcome = "Hello JavaScript - Automação QA Ninja";
+     document.getElementById("welcome").append(welcome);
+        console.log(welcome);
+
+/*
+* variável é um local na memória do computador, onde se pode alocar valores
+* valores que poderão ser alterados durante a execução do software
+* por isso é chamada de variável
+*/
+
+
+//tipagem dinamica, atriui o tipo quando recebe o valor (fracamente tipada)
+var number = 10;
+
+//imprimindo o valor
+console.log(number);
+
+//typeof, é responsável por trazer o tipo da variável
+console.log(typeof number);
